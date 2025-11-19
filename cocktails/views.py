a2400c83f4275a187d394b95e15eb5198f547f72
Kearns55 <rebekah120@hotmail.com>
2025-11-19 23:22:56 +0100

    Add django crispy forms to comments for logged in users

--- cocktails/views.py
+++ cocktails/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Cocktail
+from .forms import CommentForm
 
 
 class CocktailListView(generic.ListView):
@@ -12,11 +14,30 @@ class CocktailListView(generic.ListView):
 # Create your views here.
 def cocktail_detail(request, slug):
     """ A view to show individual cocktail details """
-    
     queryset = Cocktail.objects.all().filter(approved=True)
     cocktail = get_object_or_404(queryset, slug=slug)
+    comments = cocktail.comments.all().order_by("-created_on")
+    comment_count = cocktail.comments.filter(approved=True).count()
+    if request.method == "cocktail":
+        comment_form = CommentForm(data=request.cocktail)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.cocktail = cocktail
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+    
+    comment_form = CommentForm()
     return render(
         request,
         'cocktails/cocktail_detail.html',
-        {'cocktail': cocktail},
+        {
+            'cocktail': cocktail,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+            },
     )
--- main/settings.py
+++ main/settings.py
@@ -48,14 +48,19 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django_summernote',
     'cocktails',
-    'about',
+    # 'about',
 ]
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -67,7 +72,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    
 ]
 
 ROOT_URLCONF = 'main.urls'
