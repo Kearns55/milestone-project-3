185ae526695021c4f5f3ce6b0892f2d6f21e66a5
Kearns55 <rebekah120@hotmail.com>
2025-11-20 17:05:45 +0100

    Update gitignore file

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL",
-    "postgresql://neondb_owner:npg_JPLCOA2byfx3@ep-falling-leaf-agfywzze.c-2.eu-central-1.aws.neon.tech/canal_elf_widow_272309")
-
-os.environ.setdefault(
-    "SECRET_KEY",
-    "django-insecure-0(!!q+h5@3e4jtxcj!1ohog(@yw(1jl1on0e&-ty5)9poq6n^l")
