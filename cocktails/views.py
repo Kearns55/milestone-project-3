b979e1058dd6651610c241e2672eaf4e32756748
Kearns55 <rebekah120@hotmail.com>
2025-11-20 20:04:20 +0100

    Refactor cocktail detail view for authenticated users

--- cocktails/views.py
+++ cocktails/views.py
@@ -27,32 +27,39 @@ class CocktailListView(generic.ListView):
 
 
 # Create your views here.
-def cocktail_detail(request, slug):
+def cocktail_detail(request, pk, slug):
     """ A view to show individual cocktail details """
-    queryset = Cocktail.objects.all().filter(approved=True)
-    cocktail = get_object_or_404(queryset, slug=slug)
+    cocktail = get_object_or_404(Cocktail, pk=pk)
+    # Redirect if cocktail is not approved, except by the author/owner
+    if cocktail.author != request.user and not cocktail.approved:
+        messages.warning(
+            request,
+            'This cocktail is not approved yet.')
+        return redirect("cocktails")
+    # Continue if cocktail is approved
     comments = cocktail.comments.all().order_by("-created_on")
     comment_count = cocktail.comments.filter(approved=True).count()
-    if request.method == "cocktail":
-        comment_form = CommentForm(data=request.cocktail)
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
             comment.author = request.user
             comment.cocktail = cocktail
             comment.save()
-            messages.add_message(
-                request, messages.SUCCESS,
-                'Comment submitted and awaiting approval'
-            )
-    
+            messages.success(
+                request,
+                'Comment submitted and awaiting approval')
+            return redirect("cocktail_detail", pk=pk, slug=slug)
+        messages.error(
+            request,
+            'There was an error with your comment. Please try again.')
+        return redirect("cocktail_detail", pk=pk, slug=slug)
     comment_form = CommentForm()
-    return render(
-        request,
-        'cocktails/cocktail_detail.html',
-        {
-            'cocktail': cocktail,
-            "comments": comments,
-            "comment_count": comment_count,
-            "comment_form": comment_form,
-            },
-    )
+    template = 'cocktails/cocktail_detail.html'
+    context = {
+        'cocktail': cocktail,
+        "comments": comments,
+        "comment_count": comment_count,
+        "comment_form": comment_form,
+    }
+    return render(request, template, context)
